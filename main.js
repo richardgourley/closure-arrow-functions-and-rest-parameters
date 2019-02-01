e4eb87b2be7028588cbe3bb3fc343eddf653e778
richardgourley <39519164+richardgourley@users.noreply.github.com>
2019-02-01 16:50:17 +0100

    Moved comments from readme to main.js file.

--- main.js
+++ main.js
@@ -1,53 +1,48 @@
 
+//ARROW FUNCTIONS
+//Line * is an example of arrow functions in use.  We could have written it as:
+// let prepareMessage = function(name, res){.....}
 let prepareMessage = (name, res) => {
+    //CLOSURE
+    //In line * we set an inner function.  The inner function can be called later on our script (see below).
+    //Line * also has another example of arrow functions in use.  Could have been written as: return function(){....}
     return () => {
-        let nameUpper = name.toUpperCase();
-        let message = res>75 ? "passed" : "did't pass";
-        let extraMessage = ". What an achievement!!!";
-        let printedMessage = `${nameUpper} ${message} the test with a score of ${res}`;
-        if(res > 89){printedMessage += extraMessage;}
+        //TERNARY OPERATOR - check if the score is over 75 or not, assign var to "pass" or "didn't pass".
+        let message = res>75 ? "passed" : "didn't pass";
+        //TEMPLATE STRINGS - A way to format strings and add variables to string output.  Less concatenation required.
+        let printedMessage = `${name} ${message} the test with a score of ${res}`;
         console.log(printedMessage);
     }
-    
 }
 
+let dave = prepareMessage("Dave", 78);
+console.log(dave); //Output: dave is a function.
+//When we call 'dave' as a function, we will invoke the inner function inside our prepareMessage function.
 
-var frank = prepareMessage("Frank", 67);
-var paul = prepareMessage("Paul", 81);
-var jenny = prepareMessage("Jenny", 96);
+//Create some more students...
+let daphne = prepareMessage("Daphne", 89);
+let john = prepareMessage("John", 68);
 
+//REST PARAMETERS - This means a function can take any number of parameters as its arguments.
+//We use ...theArgs as a parameter and then perform a loop on our unspecified number of arguments.
 let printResults = function(...theArgs){
-     for(i=0; i<theArgs.length; i++){
-         let res = typeof(theArgs[i]);
-         if(res === 'function'){
-             theArgs[i]();
-         }
-     }
-}
-
-printResults(frank, paul, jenny);
-
-//Output:
-//SALLY failed the test with a score of 67.
-//PAUL passed the test with a score of 81.
-//JENNY passed the test with a score of 96. What an achievement!!!
-
-//PREPARE MESSAGE RE-WRITTEN WITHOUT ARROW FUNCTIONS
-/*
-let prepareMessage = function(name, res){
-    function functionToReturn(){
-        let nameUpper = name.toUpperCase();
-        let message = res>75 ? "passed" : "did't pass";
-        let extraMessage = ". What an achievement!!!";
-        let printedMessage = `${nameUpper} ${message} the test with a score of ${res}`;
-        if(res > 89){printedMessage += extraMessage;}
-        console.log(printedMessage);
+    for(i=0; i<theArgs.length; i++){
+        //'dave', 'daphne' and 'john' are functions to be passed in.  It's good practice to check args are functions before invoking them.
+        if(typeof(theArgs[i]) == 'function'){
+            //invoke each parameter in our Args array.
+            theArgs[i]();
+        }
     }
-    return functionToReturn;    
 }
 
-*/
+//Pass in our functions 'dave', 'daphne', 'john'.  
+//When printResults invokes the above functions, the INNER function of prepareMesaage will be invoked.
+printResults(dave, daphne, john);
 
+//OUTPUT
+//Dave passed the test with a score of 78 
+//Daphne passed the test with a score of 89 
+//John didn't pass the test with a score of 68
 
 
 
